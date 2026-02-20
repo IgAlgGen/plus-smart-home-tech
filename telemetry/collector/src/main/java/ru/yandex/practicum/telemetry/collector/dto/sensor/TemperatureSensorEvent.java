8231790dfc5d6a5c5da89f2f671eb453f5050a73
IgAlGen <genvarskiy.i.a@gmail.com>
2026-02-20 11:53:43 +0500

    1-collector-json

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/dto/sensor/TemperatureSensorEvent.java
@@ -0,0 +1,32 @@
+package ru.yandex.practicum.telemetry.collector.dto.sensor;
+
+import jakarta.validation.constraints.NotNull;
+
+public class TemperatureSensorEvent extends SensorEvent {
+
+    @NotNull
+    private Integer temperatureC;
+    @NotNull
+    private Integer temperatureF;
+
+    @Override
+    public SensorEventType getType() {
+        return SensorEventType.TEMPERATURE_SENSOR_EVENT;
+    }
+
+    public Integer getTemperatureC() {
+        return temperatureC;
+    }
+
+    public void setTemperatureC(Integer temperatureC) {
+        this.temperatureC = temperatureC;
+    }
+
+    public Integer getTemperatureF() {
+        return temperatureF;
+    }
+
+    public void setTemperatureF(Integer temperatureF) {
+        this.temperatureF = temperatureF;
+    }
+}
